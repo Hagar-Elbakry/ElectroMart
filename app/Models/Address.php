7967f0c4678171cfac2260c37b1873de6bc2cff9
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-07-03 17:55:20 +0300

    add model files for Address, Brand, Category, Order, OrderItem and Product, implement relationships

---
+++ app/Models/Address.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => "{$this->first_name} {$this->last_name}",
+        );
+    }
+}
