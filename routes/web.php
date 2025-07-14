700fe06d04828765e65a6ae4c09a5fe953b19056
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-07-15 01:51:54 +0300

    add frontend template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
+use App\Livewire\Auth\ForgetPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPage;
+use App\Livewire\CancelPage;
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\OrderPage;
+use App\Livewire\OrdersPage;
+use App\Livewire\ProductPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +28,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/products/{product}', ProductPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/orders', OrdersPage::class);
+Route::get('/orders/{order}', OrderPage::class);
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forget', ForgetPage::class);
+Route::get('/reset', ResetPage::class);
+
